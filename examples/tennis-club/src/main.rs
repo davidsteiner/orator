41218f3ed9614a970fbde25f0c119ec1f8cbea66
David Steiner <david_j_steiner@yahoo.co.nz>
2026-03-05 20:45:37 +0100

    Fix scalar setup

--- examples/tennis-club/src/main.rs
+++ examples/tennis-club/src/main.rs
@@ -1,9 +1,9 @@
 mod api;
 
+use api::*;
+use serde_json::json;
 use std::convert::Infallible;
 use std::sync::{Arc, Mutex};
-
-use api::*;
 
 struct TennisClub {
     members: Mutex<Vec<Member>>,
@@ -115,9 +115,26 @@ impl MembersApi for TennisClub {
 async fn main() {
     let api = Arc::new(TennisClub::new());
 
-    let app = members_router(api);
+    let scalar_config = json!({ "url": "/openapi.yaml", "theme": "kepler" });
+    let app = members_router(api)
+        .route(
+            "/openapi.yaml",
+            axum::routing::get(|| async {
+                (
+                    [("content-type", "application/yaml")],
+                    include_str!("../tennis-club.yaml"),
+                )
+            }),
+        )
+        .route(
+            "/docs",
+            axum::routing::get(move || async move {
+                axum::response::Html(scalar_api_reference::scalar_html_default(&scalar_config))
+            }),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on http://localhost:3000");
+    println!("API docs at http://localhost:3000/docs");
     axum::serve(listener, app).await.unwrap();
 }
