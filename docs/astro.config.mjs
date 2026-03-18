730707842b28ffb21a8ed2207966daee2f7dcabc
David Steiner <git@davidsteiner.dev>
2026-03-18 14:12:38 +0100

    Initial content for documentation site

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -12,8 +12,7 @@ export default defineConfig({
                 {
                     label: 'Guides',
                     items: [
-                        // Each item here is one entry in the navigation menu.
-                        {label: 'Example Guide', slug: 'guides/example'},
+                        {label: 'Getting started', slug: 'guides/getting-started'},
                     ],
                 },
                 {
