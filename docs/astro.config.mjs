9b75c78a4f72794d6d6560672ab774c2bcf1538b
David Steiner <git@davidsteiner.dev>
2026-03-18 14:18:18 +0100

    Add GitHub workflow to deploy documentation site

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://davidsteiner.github.io",
+    base: "/orator",
     integrations: [
         starlight({
             title: 'Orator',
